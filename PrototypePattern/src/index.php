9952b3234f96dff346046380878828fe6f6c0a9d
Nikola <perisicnikola37@gmail.com>
2024-03-15 10:01:29 +0100

    Add html layout

--- PrototypePattern/src/index.php
+++ PrototypePattern/src/index.php
@@ -6,10 +6,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Creating a new instance of ClientPrototype
 $client = new ClientPrototype('John', 30);
-echo "Original client: {$client->name}, {$client->age}" . "<br/>";
+echo "Original client instance: {$client->name}, {$client->age}" . "<br/><br/>";
 
 // Cloning the original ClientPrototype instance
 $client2 = $client->clone();
 $client2->name = 'Mark';
-$client2->age = "18";
-echo "Cloned client: {$client2->name}, {$client2->age}";
+echo "Cloned client instance with updated name: {$client2->name}, {$client2->age}";
--- SingletonPattern/src/index.php
+++ SingletonPattern/src/index.php
@@ -4,10 +4,13 @@ use MyNamespace\Classes\President;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$title = "Singleton pattern";
+include_once "../../header.html";
+
 $president1 = President::getInstance();
-echo "First president: " . $president1->name . "<br/>";
+echo "First president: " . $president1->name . "<br/><br/>";
 
 $president2 = President::getInstance();
-echo "Second president: " . $president2->name . "<br/>";
+echo "Second president is same because no new instance is made: " . $president2->name . "<br/><br/>";
 
 echo "Just one president instance was created. Both variables point to the same instance";
